1f099644e3fec9ca9297e44e55db88d61446d1b7
SandeshBhandareHere <2317053@ritindia.edu>
2025-06-09 21:47:09 +0530

    Add files via upload
    Name and Code. A mini project to find code for your name or vice versa.

---
+++ Namecode.java
@@ -0,0 +1,71 @@
+
+import java.util.Scanner;
+
+public class Namecode {
+
+    static void nametocode() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name");
+        String name = sc.nextLine();
+        System.out.println("Your name in code is");
+        name=name.toUpperCase();
+        for (int i = 0; i < name.length(); i++) {
+            char x = name.charAt(i);
+            System.out.print((int) x);
+        }
+        System.out.println("");
+
+    }
+
+    static void code() {
+        System.out.println("Enter the code to find the name");
+        Scanner sc = new Scanner(System.in);
+        long name=sc.nextLong();
+        String xname=""+name;
+        //System.out.println(xname);
+        int l = xname.length();
+        //System.out.println("lenght of string is "+l);
+        int index=0;
+        for (int i=0;i<(l/2);i++){
+            char char1=xname.charAt(index);
+            char char2=xname.charAt(index+1);
+            String char3=char1+""+char2;
+            int as=Integer.parseInt(char3);
+            System.out.print((char)as);
+            index=index+2;
+
+        }   
+                System.out.println("");
+
+        
+        
+    }
+
+    public static void main(String[] args) {
+        int cont=1;
+            while(cont==1){
+            Scanner sc = new Scanner(System.in);
+            System.out.println("1.ascii code \n2.code for name");
+            int ch = sc.nextInt();
+
+            switch (ch) {
+                case 1: 
+                    code();
+                    break;
+
+                case 2:
+                    nametocode();
+                    break;
+                default: {
+                    System.out.println("You dumb af bruh \nEnter a valid input");
+                }
+
+            }
+
+            System.out.println("enter 0 to exit or 1 to continue");
+            cont=sc.nextInt();
+
+        }
+    }
+
+}
